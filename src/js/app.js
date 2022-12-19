c88342c45cba8d1049341c04a7034c0568814b5c
Alper Yumer <112901293+alperomer66@users.noreply.github.com>
2022-12-19 23:08:43 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,17 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  const main = document.querySelector(".main");
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
   });
+  document.addEventListener("click", () => {
+    for (let i = 0; i <5;i++) {
+      const newArticle = document.createElement("article");
+      newArticle.classList ="message";
+      newArticle.textContent = "Example text";
+      document.body.appendChild(newArticle);
+    }
+  })
 });
